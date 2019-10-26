950613e0dac872aafd371030c22eda4a9cfae915
Mcortez07 <34293872+Mcortez07@users.noreply.github.com>
2019-10-25 22:05:39 -0600

    Cambios
    algunos cambio toque el model del repuesto espero no te afecte

---
+++ VentaDeRepuestosv2/Modelos/Ventas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaDeRepuestosv2.Modelos
+{
+    class Ventas
+    {
+        public string ID_VENTA { get; set; }
+        public string ID_USUARIO { get; set; }
+        public string ID_Cliente { get; set; }
+        public string NRODOC { get; set; }
+        public string ID_TIPODOC { get; set; }
+        public string FECHAVENTA { get; set; }
+        public string NRO_CF { get; set; }
+        public double SUBTOTAL { get; set; }
+        public double MONTOTOTAL { get; set; }
+        public double IVA { get; set; }
+        public double DESCUENTO { get; set; }
+    }
+}
